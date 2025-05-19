e65eaa89a60272d4bafeb51a48ca1961d49acd80
Radiuson <runqianzhang@imzrq.cn>
2025-05-19 10:39:26 +0900

    add initial implementation of YOLO model for object detection

---
+++ test.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+import torch
+
+
+model = YOLO("/home/ihpc/code/yolo/ultralytics/ultralytics/cfg/models/v8/yolov8-obb-depth.yaml",task="obb-depth")  # Load a model
+x = torch.randn(16, 3, 640, 640)
+print(model)
+y = model(x)  # Inference
+print(y)  # Predictions
