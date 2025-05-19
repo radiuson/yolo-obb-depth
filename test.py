f5bd40094e9c12696d0e1257dd7d62174822a734
Radiuson <runqianzhang@imzrq.cn>
2025-05-19 11:02:41 +0900

    Import ultralytics code as normal folder

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import torch
 
 
-model = YOLO("/home/ihpc/code/yolo/ultralytics/ultralytics/cfg/models/v8/yolov8-obb-depth.yaml",task="obb-depth")  # Load a model
+model = YOLO("/home/ihpc/code/yolo/yolo-obb-depth/ultralytics_rep/ultralytics/cfg/models/v8/yolov8-obb-depth.yaml",task="obb-depth")  # Load a model
 x = torch.randn(16, 3, 640, 640)
 print(model)
 y = model(x)  # Inference
